56c0df1d56889953bc71e7ac992ae852d4dd11eb
Austin Amaral <austin.amaral3@gmail.comgit config --listgit config --global user.email austin.amaral3@gmail.com>
2023-06-14 16:48:06 -0700

    Styling added WIP

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,23 @@
 // Write your Character component here
 import React from 'react';
+import styled from 'styled-components'
 
+const StyledDiv = styled.div`
+    padding: 6px 10px;
+    color: black;
+    margin: 5px;
+    border: green;
+    
+`
+const StyledP = styled.p`
+    color: orange;
+`
 const Character = props => {
     return (
-        <div className ="character">
-            {props.info.name}
-        </div>
+        <StyledDiv className ="character">
+            <p>{props.info.name}</p>
+            <StyledP>{props.info.birth_year}</StyledP>
+        </StyledDiv>
  )
 }
 
