7d052cbfd0b9c0548736934c2b69ea3ddbb90782
Austin Amaral <austin.amaral3@gmail.com>
2023-06-14 15:03:35 -0700

    Character almost done

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,15 +1,14 @@
 // Write your Character component here
 import react, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { handlers } from '../mocks/handlers';
-console.log(handlers)
 
 const Character = () => {
     const [character, setCharacter] = useState(null);
     useEffect(() => {
-        axios.get(handlers)
+        axios.get('https://swapi.dev/api/people/')
             .then(resp => {
                 setCharacter(resp.data)
+                // console.log(resp.data)
             })
             .catch(error => {
                 console.error('Error fetching character data:', error);
