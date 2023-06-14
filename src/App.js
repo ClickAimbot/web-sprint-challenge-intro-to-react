0b69ef38ca63abbf3c7aaee2fed7a45049261993
Austin Amaral <austin.amaral3@gmail.comgit config --listgit config --global user.email austin.amaral3@gmail.com>
2023-06-14 16:03:36 -0700

    Finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import Character from "./components/Character"
+import axios from 'axios'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [character, setCharacter] = useState([]);
+    useEffect(() => {
+        axios.get('https://swapi.dev/api/people/')
+            .then(res => {
+                setCharacter(res.data)
+            })
+            .catch(error => {
+                console.error('Error fetching character data:', error);
+            })
+    }, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -11,7 +22,10 @@ const App = () => {
 
   return (
     <div className="App">
-      <Character className="Header">Characters</Character>
+      <h1 className="Header">Characters of the force:</h1>
+      { character.map(character => {
+        return <Character info={character} />
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,26 +1,12 @@
 // Write your Character component here
-import react, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { handlers } from '../mocks/handlers';
-console.log(handlers)
+import React from 'react';
 
-const Character = () => {
-    const [character, setCharacter] = useState(null);
-    useEffect(() => {
-        axios.get('https://swapi.dev/api/people/')
-            .then(resp => {
-                setCharacter(resp.data)
-            })
-            .catch(error => {
-                console.error('Error fetching character data:', error);
-            })
-    }, [])
-
+const Character = props => {
     return (
         <div className ="character">
-            <h1>Characters of the force</h1> 
+            {props.info.name}
         </div>
-    )
+ )
 }
 
 export default Character;
