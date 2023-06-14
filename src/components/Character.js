939d1518a631859c496f6f2c119355b695af5b99
Austin Amaral <austin.amaral3@gmail.comgit config --listgit config --global user.email austin.amaral3@gmail.com>
2023-06-14 15:23:57 -0700

    updated

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,8 @@
 // Write your Character component here
 import react, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { handlers } from '../mocks/handlers';
+console.log(handlers)
 
 const Character = () => {
     const [character, setCharacter] = useState(null);
@@ -8,7 +10,6 @@ const Character = () => {
         axios.get('https://swapi.dev/api/people/')
             .then(resp => {
                 setCharacter(resp.data)
-                // console.log(resp.data)
             })
             .catch(error => {
                 console.error('Error fetching character data:', error);
@@ -17,11 +18,7 @@ const Character = () => {
 
     return (
         <div className ="character">
-            <h1>Characters of the force</h1>
-            {characters.map(character => {
-                return 
-            })}
-            
+            <h1>Characters of the force</h1> 
         </div>
     )
 }
